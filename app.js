492d436728c3ce958802eba1b4dc29b0008a8dd1
Oumar <baarryoumar@gmail.com>
2023-02-14 22:22:55 -0500

    passport packages installed

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require ('bcrypt');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -35,24 +34,7 @@ app.route('/login')
 })
 
 .post((req, res) =>{
-  const username = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({email :username}, function(err, userFound){
-    if (err){
-      console.log(err);
-    } else {
-      if (userFound){
-        console.log (userFound.password);
-        
-        bcrypt.compare(password, userFound.password, (err, result) =>{
-          if (result === true){
-            res.render('secrets')
-          }
-        });
-      }
-    }
-  })
+  
 });
 
 //---------------------------------------------------------------------------------------------//
@@ -64,23 +46,6 @@ app.route('/register')
 })
 
 .post((req, res) =>{
-
-  bcrypt.hash(req.body.password, saltRounds, (err, hash)=>{
-
-    const newUser = new User({
-      email : req.body.username,
-      password: hash,
-    });
-    
-    newUser.save((err) =>{
-      if (!err) {
-        res.render('secrets');
-      } else {
-        console.log (err.message);
-      }
-    });
-
-  })
   
 })
 
