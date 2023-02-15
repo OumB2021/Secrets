9b1effafd048750eebdbce01ddf15e58af2d6dfd
Oumar <baarryoumar@gmail.com>
2023-02-14 22:43:01 -0500

    passport initialization and set up

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-const saltRounds = 10;
+
+app.use(session({
+  secret: 'Our little secret.',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
 
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://127.0.0.1:27017/userDB', {useNewUrlParser: true});
